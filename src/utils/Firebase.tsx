f9ec8edfdd42e630cf970526ecb6292e103015df
Jonathan Casillas <react.jonathan.casillas@gmail.com>
2021-11-04 21:28:57 -0600

    modicando la base

--- src/utils/Firebase.tsx
+++ src/utils/Firebase.tsx
@@ -1,5 +1,6 @@
 
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCCeuj3FLiVXgWw_AQBN1ojw9WYzDeRIS0",
@@ -13,5 +14,5 @@ const firebaseConfig = {
 
 
 const Firebase = initializeApp(firebaseConfig);
+const database = getDatabase(Firebase);
 
-export default Firebase;
